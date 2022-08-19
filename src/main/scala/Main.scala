4eacc23614e9be98c59c3f1294aaa0916e6ead65
rohit garg <rohit.garg@hotmail.com>
2022-08-19 14:34:20 -0500

    Initial Check-In

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    //sc.parallelize([3,4,5]).flatMap(lambda x: [x, x*x]).collect()
+    println("Hello world!")
+  }
+
+  def fileLoad ():
+  Unit = {
+  }
+}

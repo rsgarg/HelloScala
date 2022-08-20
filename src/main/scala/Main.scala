f7e71b0755bbc1f5a6e5bfb3b1d83108fc1eb061
rohit garg <rohit.garg@hotmail.com>
2022-08-20 17:27:30 -0500

    Update One

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,12 @@
 // import org.apache.spark.sql.SQLContext
-object Main {
-  def main(args: Array[String]): Unit = {
+object Main extends App {
+/*  def main(args: Array[String]): Unit = {
     //val conf = new SparkConf().setMaster("local[*]")
     println("number of arguments passed" + args.length)
     //sc.parallelize([3,4,5]).flatMap(lambda x: [x, x*x]).collect()
     println("Hello world!")
-  }
-
+  }*/
+println("Heres another one")
   def fileLoad ():
   Unit = {
   }

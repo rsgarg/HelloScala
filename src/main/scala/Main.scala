4b9fd75690d5ad6cf3094467b719cfbebae43c34
rohit garg <rohit.garg@hotmail.com>
2022-08-20 14:44:54 -0500

    Update One

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,8 @@
+// import org.apache.spark.sql.SQLContext
 object Main {
   def main(args: Array[String]): Unit = {
+    //val conf = new SparkConf().setMaster("local[*]")
+    println("number of arguments passed" + args.length)
     //sc.parallelize([3,4,5]).flatMap(lambda x: [x, x*x]).collect()
     println("Hello world!")
   }
